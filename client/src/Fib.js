fea763c983d86a956635841b3355ae89cb940591
ArthurHwang <ahwang55@gmail.com>
2018-12-18 12:02:35 -0800

    added development dockerfiles

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,54 +1,55 @@
-import React, {Component} from 'react'
-import axios from 'axios'
-
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
     index: ''
-  }
+  };
 
   componentDidMount() {
     this.fetchValues();
-    this.fetchIndexes()
+    this.fetchIndexes();
   }
 
   async fetchValues() {
-    const values = await axios.get('/api/values/current')
-    this.setState({ values: values.data})
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('/api/values/all')
-    this.setState({ seenIndexes: seenIndexes.data})
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
-  handleSubmit = async(event) => {
-    event.preventDefault()
+  handleSubmit = async event => {
+    event.preventDefault();
 
     await axios.post('/api/values', {
       index: this.state.index
-    })
+    });
+    this.setState({ index: '' });
+  };
 
-    this.setState({ index: ''})
-  }
-
-  renderSeenIndex() {
-    return this.state.seenIndexes.map(({number}) => number).join(', ')
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
-    const entries = []
+    const entries = [];
 
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
-          For Index {key} I calculated {this.state.values[key]}
+          For index {key} I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
-    return entries
+
+    return entries;
   }
 
   render() {
@@ -67,8 +68,8 @@ class Fib extends Component {
         <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
-    )
+    );
   }
 }
 
-export default Fib
+export default Fib;
